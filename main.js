c089c82ba77f7527e1c83e342a4c7f5e163e8a49
Ian Vu <ianv6207@mytusd.org>
2026-03-25 11:23:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ list.grab = function() {
   console.log("Loading JSON...");
   console.log(list);
 };
-list.add = function() {};
+list.add = function() {
+  location.href = "https://github.com/ianCVu/peppyConsole/blob/main/main.js";
+};
