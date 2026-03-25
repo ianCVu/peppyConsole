80b40c9e73b494a8c9da7ae2b7a390d15954d19c
Ian Vu <ianv6207@mytusd.org>
2026-03-25 11:41:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let list = {};
+let list = {
+  "davidw6054@mytusd.org" : "7706054"
+};
 list.grab = function() {
   console.log("Loading JSON...");
   console.log(list);
@@ -7,3 +9,19 @@ list.add = function() {
   window.open("https://github.com/ianCVu/peppyConsole/blob/main/main.js", "_blank");
   console.log("Add value to JSON.");
 };
+let peppy = {};
+peppy.ssidg = function(fn) {
+  var vps = {
+    "vp1" : "2100",
+    "vp2" : "770",
+    "vp3" : "3007",
+    "vp4" : "4600",
+    "vp5" : "200"
+  };
+  var pg = {};
+  pg.g1 = vps.vp1 + fn;
+  pg.g2 = vps.vp2 + fn;
+  pg.g3 = vps.vp3 + fn;
+  pg.g4 = vps.vp4 + fn;
+  
+}
